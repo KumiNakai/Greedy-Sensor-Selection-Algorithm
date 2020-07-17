381cbf168fadfc31397cccd9cecff35e3f65742a
KumiNakai <67894135+KumiNakai@users.noreply.github.com>
2020-07-17 21:53:54 +0900

    Delete F_sensor_convex_sub.m

--- src/F_sensor_convex_sub.m
+++
@@ -1,9 +0,0 @@
-function [isensors, zhat, NT_TOL_cal, iter]=F_sensor_convex_sub(U, p, maxiteration)
-
-    [n,r]=size(U);
-    UU=[];
-    UU(:,:,1)=U(1:n,1:r); 
-    [zhat, ~, ~, ~, NT_TOL_cal, iter] = F_sensor_convex_approxnt_vec(UU, p, maxiteration);
-    isensors = find(zhat);
-
-end
