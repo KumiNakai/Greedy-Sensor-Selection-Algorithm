894a38ad52c6a0d2e46e68f4396559d46536ec83
KumiNakai <67894135+KumiNakai@users.noreply.github.com>
2020-07-17 21:53:09 +0900

    Delete F_sensor_convex.m

--- src/F_sensor_convex.m
+++
@@ -1,9 +0,0 @@
-function [time_convex, H, sensors, NT_TOL_cal, iter]=F_sensor_convex(U, p, maxiteration)
-
-    [n,~]=size(U);
-    tic;
-    [sensors, ~, NT_TOL_cal, iter]=F_sensor_convex_sub(U, p, maxiteration);
-    time_convex=toc;
-    [H]=F_calc_sensormatrix(p, n, sensors);
-    
-end
