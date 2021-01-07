42a0325e487f06737597e0d3814e3642b04bd61c
KumiNakai <67894135+KumiNakai@users.noreply.github.com>
2021-01-07 15:53:44 +0900

    Update the repository

--- src/F_calc_reconst.m
+++
@@ -1,7 +0,0 @@
-function [Zestimate] = F_calc_reconst(Xorg, H, U)
-
-    Y = H*Xorg;
-    C = H*U;
-    Zestimate = pinv(C)*Y; %B = pinv(A) returns the Moore-Penrose Pseudoinverse of matrix A.
-
-end
--- src/F_calc_sensormatrix.m
+++
@@ -1,8 +0,0 @@
-function [H]=F_calc_sensormatrix(p, n, SNSR)
-
-   H=zeros(p, n);
-   for pp=1:1:p
-      H(pp, SNSR(pp))=1;
-   end
-   
-end
